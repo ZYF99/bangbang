867c5ac401f356e3bf368cca992ddadd1f6236e4
zyffyz <994318935@qq.com>
2018-09-20 11:36:49 +0800

    你的第一次提交

---
+++ app/src/main/java/com/bangbang/home/MainActivity.java
@@ -0,0 +1,12 @@
+package com.bangbang.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.bangbang.R;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
